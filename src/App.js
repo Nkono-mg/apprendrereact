d72eef7cef0266ea8b62c259aaf79d3f03311501
Nkono <nkono209@gmail.com>
2022-02-01 17:24:15 +0100

    continuation sur changement de state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import './App.css';
-import Hello from './componsant/Hello';
-import Mycars from './componsant/Mycars';
+import "./App.css";
+import Hello from "./componsant/Hello";
+import Mycars from "./componsant/Mycars";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-   state = {
-      title : 'Catalogue de voiture'
-   }
+  state = {
+    title: "Catalogue de voiture",
+  };
 
-   changeTitle = () =>{
-      this.setState({
-         title: "Nouveau titre"
-      })
-   }
-   render() {
-      return (
-         <div className='app'>
-            <Hello />
-            <hr />
-            <Mycars  title={this.state.title}/>
-            <button onClick={this.changeTitle}>Chager le nom en dure</button>
-         </div>
-      );
-   }
+  changeTitle = () => {
+    this.setState({
+      title: "Nouveau titre",
+    });
+  };
+  changeViaParam = (param) => {
+    this.setState({
+      title: param,
+    });
+  };
+
+  changeViaInput = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+  getViaInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      title: this.inputNode.value,
+    });
+  };
+  render() {
+    return (
+      <div className="app">
+        <Hello />
+        <hr />
+        <Mycars title={this.state.title} />
+        <button onClick={this.changeTitle}>Chager le nom en dure</button>
+        <button onClick={() => this.changeViaParam("Titre  via param")}>
+          Via param
+        </button>
+        <input
+          type="text"
+          onChange={this.changeViaInput}
+          value={this.state.title}
+        />
+        <input
+          type="text"
+          name="titre"
+          ref={(node) => (this.inputNode = node)}
+        />
+        <button onClick={this.getViaInput}>getVia Input</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/componsant/Mycars.js
+++ src/componsant/Mycars.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import Cars from './Cars'; 
+import React, { Component } from "react";
+import Cars from "./Cars";
 
-class Mycars extends Component { 
-
-    addStyle = (e)=>{
-        if(e.target.classList.contains('styled')){
-            e.target.classList.remove('styled');
-        }else{
-            e.target.classList.add('styled');
-        }
+class Mycars extends Component {
+  addStyle = (e) => {
+    console.log(e);
+    if (e.target.classList.contains("styled")) {
+      e.target.classList.remove("styled");
+    } else {
+      e.target.classList.add("styled");
     }
-    render() {
-        return (
-            <div className="mycars">
-                <h1 onMouseOver={this.addStyle}>{this.props.title}</h1>
-                <Cars color="red">Ford</Cars>
-                <Cars color="">Mercedes</Cars>
-                <Cars color="Green"></Cars>
-            </div>
-        );
-    }
+  };
+  render() {
+    return (
+      <div className="mycars">
+        <h1 onMouseOver={this.addStyle}>{this.props.title}</h1>
+        <Cars color="red">Ford</Cars>
+        <Cars color="">Mercedes</Cars>
+        <Cars color="Green"></Cars>
+      </div>
+    );
+  }
 }
 export default Mycars;
